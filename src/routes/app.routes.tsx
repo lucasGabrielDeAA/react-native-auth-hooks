7a8dc2d519bb9775c14ed019b56330206f1dedfb
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-27 14:03:16 -0300

    Routes directory added

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const AppStack = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <AppStack.Navigator>
+    <AppStack.Screen name="Dashboard" component={Dashboard} />
+  </AppStack.Navigator>
+);
+
+export default AppRoutes;
